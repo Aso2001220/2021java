76af2c226b17c0ede7b40ad37f97df3c13cd5b85
Aso2001220 <83048215+Aso2001220@users.noreply.github.com>
2021-07-30 21:37:11 +0900

    Add files via upload

---
+++ Main.java/Ship.java 1.java
@@ -0,0 +1,10 @@
+public class Ship{
+    public void lastdisp(){
+        System.out.println("戦艦を全て撃退した");
+    }
+
+    public void seconddisp(){
+        System.out.println("戦艦に爆弾が当たった");
+    }
+
+}
